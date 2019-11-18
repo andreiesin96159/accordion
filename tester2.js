7eb96da5658d30458a4b727ef49d6cabdb7a93fa
andreiesin96159 <andreiesin96159@gmail.com>
2019-11-18 15:33:21 +0400

    clen

--- tester2.js
+++
@@ -1,8 +0,0 @@
-const arr = function(i) {
-    fetch('', {
-        method: 'GET',
-        body: "data",
-        headers: { "Content-Type": "text/plain;charset=UTF-8"}
-    });
-    console.log(i);
-};     console.log(arr);
--- tester3.js
+++
@@ -1,6 +0,0 @@
-import {faker} from "faker";
-import {puppeteer} from "puppeteer";
-
-const APP = "https://master-gusa-desktop.gusadev.com/checkout/onepage/";
-
-
